fce78d388723934090dccdddf89892696a3d4e58
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-11-05 16:53:22 -0300

    Parte 01
    Hellow ASP

---
+++ Asp.Net_Projeto01_HellowAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.Net_Projeto01_HellowAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
