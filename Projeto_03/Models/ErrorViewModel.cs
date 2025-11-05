d8ca6925d4a55027176e1c645ee12031616a3740
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-11-05 16:18:14 -0300

    Limpeza

--- Projeto_03/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Projeto_03.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
